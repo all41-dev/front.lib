6f9c4df9003072aa23954bfe87fac6c0482b8f51
Radmyr <radmyr.deriabin11@gmail.com>
2025-09-08 14:00:37 +0200

    fix: not generation readme.nd

--- front-lib/stencil.config.ts
+++ front-lib/stencil.config.ts
@@ -29,9 +29,6 @@ export const config: Config = {
       externalRuntime: false,
     },
     {
-      type: 'docs-readme',
-    },
-    {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
